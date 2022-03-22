74974e9a75fca35f1fc07bdef78e1b0507cf46de
Jerome <ducampdorgasjerome@gmail.com>
2022-03-22 15:19:48 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [simpsons, setSimpsons] = React.useState({
+    quote: "",
+    character: "",
+    image: "",
+  });
+
+  function getQuotes() {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data[0].quote);
+        console.log(data[0].character);
+        setSimpsons({
+          quote: data[0].quote,
+          character: data[0].character,
+          image: data[0].image,
+        });
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{simpsons.character}</p>
+      <img src={simpsons.image} alt={simpsons.character} />
+      <p>{simpsons.quote}</p>
+
+      <button onClick={getQuotes}>Cliquez-moi</button>
     </div>
   );
 }
